f923fea93322905eabe11acf3063b55ed9f75fdf
Svetlin Ralchev <svet@ralch.com>
2018-03-15 17:13:45 +0100

    Add builder aliase to loukoum

---
+++ builder.go
@@ -0,0 +1,28 @@
+package gom
+
+import (
+	"github.com/ulule/loukoum"
+)
+
+var (
+	InnerJoin = loukoum.InnerJoin
+	LeftJoin  = loukoum.LeftJoin
+	RightJoin = loukoum.RightJoin
+	Asc       = loukoum.Asc
+	Desc      = loukoum.Desc
+	And       = loukoum.And
+	Column    = loukoum.Column
+	Condition = loukoum.Condition
+	Delete    = loukoum.Delete
+	DoNothing = loukoum.DoNothing
+	DoUpdate  = loukoum.DoUpdate
+	Insert    = loukoum.Insert
+	On        = loukoum.On
+	Or        = loukoum.Or
+	Order     = loukoum.Order
+	Pair      = loukoum.Pair
+	Raw       = loukoum.Raw
+	Select    = loukoum.Select
+	Table     = loukoum.Table
+	Update    = loukoum.Update
+)
