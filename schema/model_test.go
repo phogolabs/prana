f0cf276cc27073185ea1951873a39f2c6c750b75
Svetlin Ralchev <svet@ralch.com>
2018-04-15 16:44:30 +0200

    Improve test coverage for schema model

---
+++ schema/model_test.go
@@ -0,0 +1,103 @@
+package schema_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/phogolabs/oak/schema"
+)
+
+var _ = Describe("Model", func() {
+	Describe("FieldTag", func() {
+		It("formats the tag successfully", func() {
+			tag := &schema.FieldTag{Name: "validate"}
+			tag.AddOption("required")
+			Expect(tag.String()).To(Equal("validate:\"required\""))
+		})
+
+		Context("when the tag does not have options", func() {
+			It("formats the tag successfully", func() {
+				tag := &schema.FieldTag{Name: "validate"}
+				Expect(tag.String()).To(Equal("validate:\"-\""))
+			})
+		})
+	})
+
+	Describe("FieldTagList", func() {
+		It("formats all tags successfully", func() {
+			tags := schema.FieldTagList{}
+			tags = append(tags, &schema.FieldTag{Name: "tag1"})
+			tags = append(tags, &schema.FieldTag{Name: "tag2"})
+			Expect(tags.String()).To(Equal("`tag1:\"-\" tag2:\"-\"`"))
+		})
+	})
+
+	Describe("TypeDef", func() {
+		It("returns the type name", func() {
+			def := &schema.TypeDef{Type: "int"}
+			Expect(def.As(false)).To(Equal("int"))
+		})
+
+		Context("when the type is nullable", func() {
+			It("returns the nullable type name", func() {
+				def := &schema.TypeDef{
+					Type:         "int",
+					NullableType: "null.int",
+				}
+				Expect(def.As(true)).To(Equal("null.int"))
+			})
+		})
+	})
+
+	Describe("ColumnType", func() {
+		var columnType schema.ColumnType
+
+		BeforeEach(func() {
+			columnType = schema.ColumnType{
+				Name:          "varchar",
+				IsPrimaryKey:  true,
+				IsNullable:    true,
+				CharMaxLength: 200,
+			}
+		})
+
+		It("returns the column type as string correctly", func() {
+			Expect(columnType.String()).To(Equal("VARCHAR(200) PRIMARY KEY NULL"))
+		})
+
+		Context("when the type is not null", func() {
+			BeforeEach(func() {
+				columnType.IsNullable = false
+			})
+
+			It("returns the column type as string correctly", func() {
+				Expect(columnType.String()).To(Equal("VARCHAR(200) PRIMARY KEY NOT NULL"))
+			})
+		})
+
+		Context("when the type has precision and scale", func() {
+			BeforeEach(func() {
+				columnType.CharMaxLength = 0
+				columnType.Name = "numeric"
+				columnType.Precision = 10
+				columnType.PrecisionScale = 20
+			})
+
+			It("returns the column type as string correctly", func() {
+				Expect(columnType.String()).To(Equal("NUMERIC(10, 20) PRIMARY KEY NULL"))
+			})
+		})
+
+		Context("when the type has precision only", func() {
+			BeforeEach(func() {
+				columnType.CharMaxLength = 0
+				columnType.Name = "numeric"
+				columnType.Precision = 10
+				columnType.PrecisionScale = 0
+			})
+
+			It("returns the column type as string correctly", func() {
+				Expect(columnType.String()).To(Equal("NUMERIC(10) PRIMARY KEY NULL"))
+			})
+		})
+	})
+})
