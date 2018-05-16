71b55dfabfd34a1e1f7c9ee0b100fbcf3b347704
Svetlin Ralchev <svet@ralch.com>
2018-05-16 11:20:18 +0300

    Remove uneeded types

--- sqlexec/model.go
+++ sqlexec/model.go
@@ -11,16 +11,12 @@ var (
 	format = "20060102150405"
 )
 
-// Rows is a wrapper around sql.Rows which caches costly reflect operations
-// during a looped StructScan.
-type Rows = sqlx.Rows
-
 // Param is a command parameter for given query.
 type Param = interface{}
 
-// P is a shortcut to a map. It facilitates passing named params to a named
-// commands and queries
-type P = map[string]Param
+// Rows is a wrapper around sql.Rows which caches costly reflect operations
+// during a looped StructScan.
+type Rows = sqlx.Rows
 
 // FileSystem provides with primitives to work with the underlying file system
 type FileSystem = parcello.FileSystem
