adf8970cce5adb620860d605de3f9bfc04f8f41f
Svetlin Ralchev <svet@ralch.com>
2018-05-18 19:12:08 +0300

    Bump beta version

--- cmd/prana/main.go
+++ cmd/prana/main.go
@@ -50,7 +50,7 @@ func main() {
 		HelpName:             "prana",
 		Usage:                "Golang Database Manager",
 		UsageText:            "prana [global options]",
-		Version:              "1.0-beta",
+		Version:              "1.0-beta-01",
 		BashComplete:         cli.DefaultAppComplete,
 		EnableBashCompletion: true,
 		Writer:               os.Stdout,
